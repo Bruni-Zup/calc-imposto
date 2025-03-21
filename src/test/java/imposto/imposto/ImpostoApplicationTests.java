245f0255779a9d5d472a80d46140480c395b70e5
Bruni Zup <bruniely.marchioro@zup.com.br>
2025-03-21 08:36:36 -0300

    Adiciona teste básico para o carregamento do contexto da aplicação ImpostoApplication

---
+++ src/test/java/imposto/imposto/ImpostoApplicationTests.java
@@ -0,0 +1,12 @@
+package imposto.imposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class
+ImpostoApplicationTests {
+		@Test
+	void contextLoads() {}
+
+}
